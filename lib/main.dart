1cc78b1448282615b7f7cc6c7ccda6a00426b51b
JoelRobinsonUK <joelsgrobinson@gmail.com>
2025-06-12 17:03:17 +0100

    Added firebase connections

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:recipe_app/firebase_options.dart';
 import 'package:recipe_app/home.dart';
 import 'package:recipe_app/theme.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
